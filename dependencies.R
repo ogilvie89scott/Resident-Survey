03e79b842d45f77a5f92b00cb5bb0eb7f0185af3
ogilvie89scott <101666380+ogilvie89scott@users.noreply.github.com>
2022-05-24 15:52:09 -0500

    Add files via upload

--- dependencies.R
+++ dependencies.R
@@ -1,10 +1,41 @@
+# ********** Start of Header **************
+# Title: Dependencies for Resident Survey app
+#
+# This code tells the app what packages need to be downloaded.
+#
+# Author: Scott Ogilvie
+# Date: 05/25/2022
+#
+# *********** End of header ****************
 # LIST OF REQUIRED PACKAGES -----------------------------------------------
-
 # Package names
-packages <- c("broom", "data.table", "DT", "CGPfunctions", "ggridges", "leaflet", "lintr",
-              "lubridate", "plotly", "lubridate", "rintrojs", "shiny",
-              "shinycssloaders", "shinydashboard", "shinyjs", "shinyWidgets",
-              "ggpubr", "tidyverse", "rcompanion", "viridis", "zoo")
+packages <-
+  c(
+    "broom",
+    "data.table",
+    "DT",
+    "CGPfunctions",
+    "ggridges",
+    "leaflet",
+    "lintr",
+    "lubridate",
+    "plotly",
+    "lubridate",
+    "rintrojs",
+    "shiny",
+    "shinycssloaders",
+    "shinydashboard",
+    "shinyjs",
+    "shinyWidgets",
+    "surveydata",
+    "ggpubr",
+    "tidyverse",
+    "rcompanion",
+    "viridis",
+    "zoo",
+    "recipes",
+    "readxl"
+  )
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -14,4 +45,3 @@ if (any(installed_packages == FALSE)) {
 
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
-
